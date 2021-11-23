21353c5a177ef7341ae61ce621cb214101678645
sham912411 <shamalhakim91@gmail.com>
2021-11-23 10:45:00 +0100

    aufgabe

---
+++ function.js
@@ -0,0 +1,20 @@
+/*
+Katze
+*/
+let miauen = true;
+console.log ("Miau, miau, miau");
+
+function miauen () {
+    console.log ("Miau, miau, miau");
+}
+
+function bark() {
+    if(miauen){
+    } else {
+    console.log("Miau, miau, miau");
+    }
+    /*
+     die Katze, mit der sie eine Maus fangen kann. 
+     */
+    let isMouseAlive = true;
+    
