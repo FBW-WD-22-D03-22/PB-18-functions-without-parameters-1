09a0b0d0fa66cb25894b0c9db2c913e14191d6f4
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-23 10:06:28 +0100

    Orhan's lösung

---
+++ function_without_parameters.js
@@ -0,0 +1,23 @@
+let isMouseAlive = true;
+
+function miau() {
+    console.log("Aufgabe 1 ) Miau, miau, miau");
+}
+
+function catchMouse() {
+    let isMouseAlive = false;
+    console.log("Aufgabe 2 ) Is mouse a live? =", isMouseAlive);
+}
+
+function treeClimbing() {
+    if (isMouseAlive == true || isMouseAlive == false) {
+        console.log("Aufgabe 3a ) Die Katze versteckt sich vor der Maus");
+    } else {
+        console.log("Aufgabe 3b ) Die Katze ruht sich aus");
+    }
+}
+
+miau();
+catchMouse();
+treeClimbing();
+
