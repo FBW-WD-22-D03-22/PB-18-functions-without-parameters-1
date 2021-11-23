c4b153700ba6c5da4de68db78af4b8c4998a0b3e
sprotte <metatron1972@live.de>
2021-11-23 09:58:03 +0100

    sprotte

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function myFunction() {
 // function schreiben
 let isMouseAlive = true
 
+
 function miau() {console.log("miau, miau, miau");
 }
 
@@ -25,6 +26,8 @@ function fangeineMaus() {console.log("...Quitsch");
 function aufDenBaum() {console.log("oben auf den Baum");
 
 }
+console.log("Die Katze ruht sich aus");
+
 miau();
 fangeineMaus();
 aufDenBaum();
