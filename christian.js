8da2459813ab88e014a4acf8106f608c707f2700
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-23 11:26:55 +0100

    Lösung von Christian, mit Kommentaren

--- christian.js
+++ christian.js
@@ -1,22 +1,29 @@
 let isMouseAlive = true; // Maus ist am Leben
 
+// 1.)
 function meow() {
   console.log("Miau, miau, miau!");
 }
 
+// 2.)
 function catchMouse() {
   isMouseAlive = false;
   console.log("Katze hat Maus gefangen.");
 }
 
+// 3.)
 function climbTree() {
-  if (isMouseAlive) {
+  if (isMouseAlive == true) {
     console.log("Die Katze versteckt sich vor der Maus auf dem Baum.");
   } else {
+    // tritt automatisch ein, wenn isMouseAlive == false
     console.log("Die Katze ruht sich auf dem Baum aus.");
   }
 }
 
 meow();
 catchMouse();
+climbTree();
+console.log("-------- Teste anderen Fall von climbTree() --------");
+isMouseAlive = true;
 climbTree();
