8b3dd5cf0a9a4292a5f745e95bc437475142a639
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-23 21:38:05 +0100

    Die lösung von Aisha

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 
-let isMouseAlive = true;
-
-
-// 1)
+ let isMouseAlive = true
+ // 1)
 function miauen() {
-    console.log("miau , miau ,miau");
+       console.log("miau , miau ,miau");
     
-}
-// 2)
- function mausFangen() {
-     let isMouseAlive = false;
+ }
+// // 2)
+  function mausFangen() {
+          isMouseAlive = false;
+          console.log("Is Mouse alive?:", isMouseAlive);
      
  }
 
-// 3)
- function baumKlettern() {
+// // 3)
+  function baumKlettern() {
 
      if (isMouseAlive){
 
-         console.log("Die Katze versteckt sich vor der Maus")
+          console.log("Die Katze versteckt sich vor der Maus")
 
-     }else{ console.log("Die Katze ruht sich aus")
+      }else{ console.log("Die Katze ruht sich aus")
 
      }
      }
      
- miauen()
- mausFangen()
- baumKlettern()
+  miauen()
+//   mausFangen()
+  baumKlettern()
+
+
+
+
+
+
+
+
