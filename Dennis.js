45fa7125eeb172cac1f56a24934924cf189e6fd3
Dennisbullys <d.rochus@hotmail.de>
2021-11-23 10:13:24 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,31 @@
+console.log("Katze");
+
+let isMouseAlive = false;
+
+
+// Aufgabenstellung - Teil 1
+function miau() {
+    console.log("miau,miau,miau");
+}
+// Aufgabenstellung - Teil 2
+
+function catchMaus(){
+    
+console.log ("Katze fängt die Maus")
+}
+
+// Aufgabenstellung - Teil 3
+function klimbing() {
+  if (isMouseAlive) {
+    console.log("Die Katze versteckt sich vor der Maus");
+  } else {
+    console.log("Die Katze ruht sich aus");
+  }
+}
+
+
+
+
+miau();
+catchMaus();
+klimbing();
