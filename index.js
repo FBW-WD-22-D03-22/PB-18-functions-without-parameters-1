4147776384dabc52342ccd924b554243658cd0ee
sprotte <metatron1972@live.de>
2021-11-23 09:33:58 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,27 @@
+
+/*
+let erssteZahl = 2;
+let zweiteZahl = 3;
+
+function substraktion() {
+  let substraktion = ersteZahl - zweiteZahl;
+  console.log("Die substraktion ist:",substraktion);
+
+} 
+function myFunction() {
+    console.log("Hallo Welt");
+}
+*/
+
+// function schreiben
+let bellen = true
+
+function miau() {console.log("miau, miau, miau");
+}
+
+function sitz() {console.log("...Hund sitzt");
+}
+
+function holBall() {console.log("hole den Ball");
+}
+bellen();
